ab18a723a15ba5626341ece2e35b23c38d22a319
nferdous2 <noorjahanferdous029@gmail.com>
2021-11-17 11:17:22 +0600

    first

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yhxur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Running Tour server')
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("tourUser");
+        const servicesCollection = database.collection("services");
+        const addOrdersCollection = database.collection("orders");
+
+        // get api
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        //post service
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('post api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+        //for orders
+        app.get('/orders', async (req, res) => {
+            const cursor = addOrdersCollection.find({});
+            const orders = await cursor.toArray();
+            console.log('orders')
+            res.json(orders);
+        });
+        //add booking
+        app.post('/addBooking', async (req, res) => {
+            const service = req.body;
+            console.log('post api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+        //delete booking api
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    client.close();
+});
+
+run().catch(console.dir);
+app.listen(port, () => {
+    console.log('Running ', port);
+})
